3b41ee0120b172f4722bb78cef8b07fb91aa3cf1
bahrom2101 <jabborov0121@gmail.com>
2023-05-14 16:49:42 +0500

    change locale bug fix

--- lib/src/features/main/presentation/widgets/app_drawer.dart
+++ lib/src/features/main/presentation/widgets/app_drawer.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:formz/formz.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:qaza_tracker/generated/locale_keys.g.dart';
 import 'package:qaza_tracker/src/config/constants/constants.dart';
